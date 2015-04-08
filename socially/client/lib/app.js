3302ca6c424a5ced4545d7cc0b13f21d63f4c817
Alysson Anthony <alymenbr@gmail.com>
2015-04-08 00:27:08 -0300

    start step 12

--- socially/client/lib/app.js
+++ socially/client/lib/app.js
@@ -1,2 +1,11 @@
 // MODULES 
-	angular.module('socially', ['angular-meteor', 'ui.router']);	
+angular.module('socially',['angular-meteor', 'ui.router']);
+
+function onReady() {
+  angular.bootstrap(document, ['socially']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
