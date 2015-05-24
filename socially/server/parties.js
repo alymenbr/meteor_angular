0f078664c65ae85e0f2c45cce573b95d15afdb56
Alysson Anthony <alymenbr@gmail.com>
2015-05-24 20:19:51 -0300

    step12 - reactiv search

--- socially/server/parties.js
+++ socially/server/parties.js
@@ -1,4 +1,7 @@
 Meteor.publish("parties", function (options, searchString) {
+  if (searchString == null)
+    searchString = '';
+
   Counts.publish(this, 'numberOfParties', Parties.find({
     'name' : { '$regex' : '.*' + searchString || '' + '.*', '$options' : 'i' },
     $or:[
@@ -10,8 +13,7 @@ Meteor.publish("parties", function (options, searchString) {
         {owner: this.userId},
         {owner: {$exists: true}}
       ]}
-  ]}), { noReady: true });  
-
+    ]}), { noReady: true });
   return Parties.find({
     'name' : { '$regex' : '.*' + searchString || '' + '.*', '$options' : 'i' },
     $or:[
@@ -23,5 +25,5 @@ Meteor.publish("parties", function (options, searchString) {
         {owner: this.userId},
         {owner: {$exists: true}}
       ]}
-    ]}, options);
+    ]} ,options);
 });
