fc9d35c41360c88a1d976d17f1930e3bb5b6bb4d
Alysson Anthony <alymenbr@gmail.com>
2015-05-24 20:49:32 -0300

    step13 - filters

--- socially/client/parties/controllers/partiesList.js
+++ socially/client/parties/controllers/partiesList.js
@@ -1,4 +1,4 @@
-	angular.module('socially').controller("PartiesListCtrl", function ($scope, $meteor) {
+	angular.module('socially').controller("PartiesListCtrl", function ($scope, $meteor, $rootScope) {
 
 			$scope.sort = { name: 1 };
 			$scope.orderProperty = '1';
@@ -14,6 +14,7 @@
 				});
 			});
 
+  			$meteor.subscribe('users');
 
 			$meteor.autorun($scope, function() {
 
@@ -43,4 +44,23 @@
 			  if ($scope.orderProperty)
 			    $scope.sort = {name: parseInt($scope.orderProperty)};
 			});
+
+			$scope.getUserById = function(userId){
+			  return Meteor.users.findOne(userId);
+			};
+
+			$scope.creator = function(party){
+			  if (!party)
+			    return;
+			  var owner = $scope.getUserById(party.owner);
+			  if (!owner)
+			    return "nobody";
+
+			  if ($rootScope.currentUser)
+			    if ($rootScope.currentUser._id)
+			      if (owner._id === $rootScope.currentUser._id)
+			        return "me";
+
+			  return owner;
+			};					
 		});
